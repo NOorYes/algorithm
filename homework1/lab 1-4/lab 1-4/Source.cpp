4748603fd9b2fca17d25b03b597bd29b2275b855
NOorYes <1313susu@gmail.com>
2017-03-23 01:14:38 +0900

    주석달아욧

--- homework1/lab 1-4/lab 1-4/Source.cpp
+++ homework1/lab 1-4/lab 1-4/Source.cpp
@@ -10,26 +10,26 @@ typedef int itemType;
 class Stack1
 {
 private:
-	itemType *stack;
-	int p;
+	itemType *stack; // int 형 포인터
+	int p; // 스택의 가장 위 인덱스를 나타낸다.
 public:
-	Stack1(int max = 100)
+	Stack1(int max = 100) // 생성자
 	{
 		stack = new itemType[max]; p = 0;
 	}
-	~Stack1()
+	~Stack1() // 소멸자
 	{
 		delete stack;
 	}
-	inline void push(itemType v)
+	inline void push(itemType v) // 스택 안으로 집어넣는다.
 	{
 		stack[p++] = v;
 	}
-	inline itemType pop()
+	inline itemType pop() // 스택 밖으로 빼낸다.
 	{
 		return stack[--p];
 	}
-	inline int empty()
+	inline int empty() // 스택이 비었는지 아닌지 나타낸다.
 	{
 		return !p;
 	}
@@ -38,22 +38,22 @@ public:
 int main() { // 스택을 이용한 후위 연산.
 	
 	char c; 
-	Stack1 acc(50); 
+	Stack1 acc(50); // 50개의 숫자를 저장할 수 있는 스택 선언.
 	int x;
 	
-	while ((c = cin.get()) != '\n')
+	while ((c = cin.get()) != '\n') // 공백 (끝) 일 때까지 버퍼에서 읽어들인다.
 	{
 		x = 0;
-		while (c == ' ') cin.get(c);
-		if (c == '+') x = acc.pop() + acc.pop();
-		if (c == '*') x = acc.pop() * acc.pop();
+		while (c == ' ') cin.get(c); // 공백이면 숫자일 때까지 읽어들인다.
+		if (c == '+') x = acc.pop() + acc.pop(); 
+		if (c == '*') x = acc.pop() * acc.pop(); // 계산 기호이면 스택에서 숫자들을 꺼내 계산한다.
 		while (c >= '0' && c <= '9')
 		{
-			x = 10 * x + (c - '0');  cin.get(c);
+			x = 10 * x + (c - '0');  cin.get(c); // 숫자일 때까지 읽어들인다. + 두자릿수 계산
 		}
-		acc.push(x);
+		acc.push(x); 
 	}
-	cout << acc.pop() << '\n';
+	cout << acc.pop() << '\n'; // 맨 마지막에 들어간 최종적인 값을 꺼내 출력한다.
 }
 
 // 입력 데이터:   9  10  15  4  *  +  20  *  7  + 
